c7140d0d70e9aa5251cccfca465344fe360f69d5
Alex Castro <acastro24@fordham.edu>
2021-11-10 16:10:20 -0500

    Update candidate.py
    Added support for the new table fields. Removed unused username field.

--- models/schemas/candidate.py
+++ models/schemas/candidate.py
@@ -4,9 +4,9 @@ import sqlite3
 class Candidate():
 
     tablename = "candidate"
-    dbpath = "../data/test.db"
+    dbpath = "../data/database.db"
 
-    def __init__(self, email, pass_hash, session_id, id = None, first_name = None, last_name = None, phone = None, description = None):
+    def __init__(self, id = None, first_name = None, last_name = None, email,  phone = None, description = None, pass_hash, session_id, ethnicity_id = None, gender_id = None, gender_pronoun_id = None):
         self.id = id
         self.pass_hash = pass_hash
         self.session_id = session_id
@@ -15,15 +15,17 @@ class Candidate():
         self.email = email
         self.phone = phone
         self.description = description
-
+        self.ethnicity_id = ethnicity_id
+        self.gender_id = gender_id
+        self.gender_pronoun_id = gender_pronoun_id
 
     def insert_candidate(self):
         with sqlite3.connect(self.dbpath) as conn:
             cursor = conn.cursor()
             sql = f"""INSERT INTO {self.tablename}
-                      (first_name, last_name, email, phone, description, pass_hash, session_id)
-                      VALUES (?,?,?,?,?,?,?);"""
-            data = (self.first_name, self.last_name, self.email, self.phone, self.description, self.pass_hash, self.session_id)
+                      (first_name, last_name, email, phone, description, pass_hash, session_id, ethnicity_id, gender_id, gender_pronoun_id)
+                      VALUES (?,?,?,?,?,?,?,?,?,?,?);"""
+            data = (self.first_name, self.last_name, self.email, self.phone, self.description, self.pass_hash, self.session_id, self.ethnicity_id, self.gender_id, self.gender_pronoun_id)
             cursor.execute(sql, data)
 
 
@@ -37,59 +39,30 @@ class Candidate():
                           phone = ?,
                           description = ?,
                           pass_hash = ?,
-                          session_id = ?
+                          session_id = ?,
+                          ethnicity_id = ?,
+                          gender_id = ?,
+                          gender_pronoun_id = ?
                       WHERE id = ?
                     """
-            data = (self.first_name, self.last_name, self.email, self.phone, self.description, self.pass_hash, self.session_id, self.id)
+            data = (self.first_name, self.last_name, self.email, self.phone, self.description, self.pass_hash, self.session_id, self.ethnicity_id, self.gender_id, self.gender_pronoun_id, self.id)
             cursor.execute(sql, data)
 
     @classmethod
-    def get_candidate(cls, criteria, data):
-        print(criteria, data)
-        if criteria == "email":
-            return Candidate.query_candidate("email", data)
-        elif criteria == "session_id":
-            return Candidate.query_candidate("session_id", data)
-        elif criteria == "candidate_id":
-            return Candidate.query_candidate("id", data)
-
-
-    @classmethod
-    def query_candidate(cls, criteria, data):
+    def get_candidate(cls, id):
         with sqlite3.connect(cls.dbpath) as conn:
             cursor = conn.cursor()
             sql = f"""SELECT *
                     FROM {cls.tablename}
-                    WHERE {criteria} = ?"""
-            cursor.execute(sql, (data,))
+                    WHERE id = ?"""
+            cursor.execute(sql, (id,))
         res =  cursor.fetchone()
         if res:
-            user = Candidate(id = res[0], first_name=res[1], last_name=res[2],email=res[3],\
-                             phone=res[4],description=res[5],pass_hash=res[6],session_id=res[7])
+            user = Candidate(id = res[0], first_name=res[1],last_name=res[2],email=res[3],phone=res[4],description=res[5],pass_hash=res[6],session_id=res[7],ethnicity_id=res[8],gender_id=res[9],gender_pronoun_id=res[10])
             return user
         return None
 
-
-    # @classmethod
-    # def get_candidate_by_session_id(cls, session_id):
-    #     with sqlite3.connect(cls.dbpath) as conn:
-    #         cursor = conn.cursor()
-    #         sql = f"""SELECT *
-    #                 FROM {cls.tablename}
-    #                 WHERE session_id = ?"""
-    #         cursor.execute(sql, (session_id,))
-    #     res =  cursor.fetchone()
-    #     if res:
-    #         user = Candidate(id = res[0], first_name=res[1], last_name=res[2],email=res[3],\
-    #                          phone=res[4],description=res[5],pass_hash=res[6],session_id=res[7])
-    #         return user
-    #     return None
-
-
-    #################################################
-    # For debug -->
     # This function should be disabled in production.
-    ##################################################
     @classmethod
     def get_all_candidates(cls):
         with sqlite3.connect(cls.dbpath) as conn:
@@ -97,20 +70,4 @@ class Candidate():
             sql = f"""SELECT *
                     FROM {cls.tablename}"""
             cursor.execute(sql)
-        candidates =  cursor.fetchall()
-        res = []
-        candidates_dict = {}
-        for candidate in candidates:
-            if candidate[1] is None or candidate[2] is None:
-                continue
-            else:
-                candidates_dict["id"] = candidate[0]
-                candidates_dict["first_name"] = candidate[1]
-                candidates_dict["last_name"] = candidate[2]
-                candidates_dict["email"] = candidate[3]
-                candidates_dict["phone"] = candidate[4]
-                candidates_dict["description"] = candidate[5]
-            res.append(candidates_dict)
-        return res
-
-
+        return cursor.fetchall()
